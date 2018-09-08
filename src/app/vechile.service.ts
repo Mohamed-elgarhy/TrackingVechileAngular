c6c5031b5f63678f7ef7d5262e9f550284571dfe
mohamed ezzeldin <m_elgarhy@yahoo.com>
2018-09-09 00:24:46 +0200

    fix observable issue

--- src/app/vechile.service.ts
+++ src/app/vechile.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Vechile}  from './models/vechile.model';
-import {Observable}  from 'rxjs/observable'
+import {Observable}  from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
